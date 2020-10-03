df7401311a5c058c4e5e989336981a6862fd49e4
Mimi C <mina123fire@yahoo.com>
2020-10-03 08:29:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Dog
+  def initialize(bark="woof!")
+    @bark = bark
+    
+  end
+  def name=(new_name)
+    @name=new_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def bark
+    @bark
+  end
+end
